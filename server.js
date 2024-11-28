0894b92a71a74ac384c87097ebae9b56e447c412
Jessé Amorim <jessecaramorim@gmail.com>
2024-11-27 22:58:14 -0300

    Foi adcionado a opcao de buscar todos, e atualiza do a opcao de usar por indice.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express from "express";
 
 const posts = [
     
-  { id: 1, descricao: "Uma foto teste", image: "https://placecats.com/millie/300/150" },
+  { id: 1, 
+    descricao: "Uma foto teste",
+    image: "https://placecats.com/millie/300/150" },
   { id: 2, descricao: "Gato fazendo yoga", image: "https://placecats.com/millie/300/150" },
   { id: 3, descricao: "Gato fazendo panqueca", image: "https://placecats.com/millie/300/150" },
   // ... outros posts com o ID no início
@@ -15,6 +17,10 @@ app.listen(3000, () => {
     console.log("Servidor Escutando...");
 });
 
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
 function buscarPostPorID(id){
     return posts.findIndex((post) => {
         return post.id === Number(id)
@@ -22,6 +28,7 @@ function buscarPostPorID(id){
 }
 
 app.get("/posts/:id", (req, res) => {
-    res.status(200).json(posts);
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
     
 });
