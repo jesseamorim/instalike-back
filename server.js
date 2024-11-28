15b1273920f6951464fb6fb49626f6a15c6f3413
Jessé Amorim <jessecaramorim@gmail.com>
2024-11-27 22:18:36 -0300

    Foi colocado id no código.

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from "express";
 
+const posts = [
+    
+  { id: 1, descricao: "Uma foto teste", image: "https://placecats.com/millie/300/150" },
+  { id: 2, descricao: "Gato fazendo yoga", image: "https://placecats.com/millie/300/150" },
+  { id: 3, descricao: "Gato fazendo panqueca", image: "https://placecats.com/millie/300/150" },
+  // ... outros posts com o ID no início
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor Escutando...");
 });
 
-app.get("/api", (req, res)=>{
-    res.status(200).send("Boas vindas a imersão!");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
